58ac90f02ea6425363909d6c75a6da90d196dd87
Subhrajit Bhowmik <39183617+Subhrajitbw@users.noreply.github.com>
2023-08-20 13:55:57 +0530

    Update ChartsAndMaps.tsx

--- src/components/ChartsAndMaps.tsx
+++ src/components/ChartsAndMaps.tsx
@@ -147,7 +147,7 @@ const Dashboard: React.FC = () => {
   const renderLeafletMap = () => {
     return (
       <div className="w-full md:w-1/2 p-4">
-        <MapContainer center={[position.lat, position.long]} zoom={4} style={{ height: "75vh", width: "50vh" }}>
+        <MapContainer center={[position.lat, position.long]} zoom={4} style={{ height: "75vh", width: "100%" }}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           {countryData.map((country) => (
             <Marker
